3ce6742f4d828d672f0e7a3e6f964ba12dd7248f
AidanSteinhauser <lightningbeans100@gmail.com>
2025-02-15 15:10:09 -0600

    310 on 2/15

--- TeamCode/src/main/java/pedroPathing/autoTest2.java
+++ TeamCode/src/main/java/pedroPathing/autoTest2.java
@@ -33,7 +33,6 @@ public class autoTest2 extends OpMode {
     private int pathState;
     private final Pose startPose = new Pose(135, 80.4, Math.toRadians(180));
     private final Pose firstPosePreOuttake = new Pose(110, 78, Math.toRadians(180));
-    //TODO OUTTAKE ONE
     private final Pose secondPoseOuttake = new Pose(106.5, 78, Math.toRadians(180));
     private final Pose thirdPosePrePushControlOne = new Pose(131, 117.5, Math.toRadians(0));
     private final Pose thirdPosePrePushControlTwo = new Pose(79, 102.5, Math.toRadians(0));
@@ -46,40 +45,7 @@ public class autoTest2 extends OpMode {
     private final Pose seventhPosePrePushThree = new Pose(82, 135.5, Math.toRadians(0));
     private final Pose eighthPosePostPushThree = new Pose(128, 135.5, Math.toRadians(0));
     private final Pose ninthPosePreIntakeOne = new Pose(127, 113, Math.toRadians(90));
-    //TODO intake one
-    private final Pose tenthPoseIntakeOne = new Pose(137, 113, Math.toRadians(90));
-    private final Pose eleventhPosePostIntakeOne = new Pose(126.5, 113, Math.toRadians(90));
-    private final Pose eleventhPosePostIntakeOneTurned = new Pose(126.5, 113, Math.toRadians(270));
-    private final Pose twelfthPosePreOuttakeTwoControlOne = new Pose(135, 73, Math.toRadians(270));
-    private final Pose twelfthPosePreOuttakeTwo = new Pose(112, 75, Math.toRadians(270));
-    //TODO OUTTAKE TWO
-    private final Pose thirteenthPoseOuttakeTwo = new Pose(109.25, 75, Math.toRadians(270));
-    private final Pose fourteenthPosePreIntakeTwoControlOne = new Pose(130, 74, Math.toRadians(90));
-    private final Pose fourteenthPosePreIntakeTwo = new Pose(125, 113, Math.toRadians(94));
-    //TODO intake two
-    private final Pose fifteenthPoseIntakeTwo = new Pose(135.25, 113, Math.toRadians(94));
-    private final Pose sixteenthPosePostIntakeTwo = new Pose(126, 113, Math.toRadians(90));
-    private final Pose sixteenthPosePostIntakeTwoTurned = new Pose(126.5, 113, Math.toRadians(270));
-    private final Pose seventeenthPosePreOuttakeThreeControlOne = new Pose(135, 74, Math.toRadians(270));
-    private final Pose seventeenthPosePreOuttakeThree = new Pose(115, 66, Math.toRadians(270));
-    //TODO OUTTAKE THREE
-    private final Pose eighteenthPoseOuttakeThree = new Pose(109.5, 66, Math.toRadians(270));
-    private final Pose nineteenthPosePreIntakeThreeControlOne = new Pose(130, 74, Math.toRadians(90));
-    private final Pose nineteenthPosePreIntakeThree = new Pose(127, 113, Math.toRadians(95));
-    //TODO intake three
-    private final Pose twentiethPoseIntakeThree = new Pose(134.8, 113, Math.toRadians(95));
-    private final Pose twentyFirstPostPoseIntakeThree = new Pose(126.5, 113, Math.toRadians(95));
-    private final Pose twentyFirstPostPoseIntakeThreeTurned = new Pose(126.5, 113, Math.toRadians(270));
-    private final Pose twentySecondPosePreOuttakeFourControlOne = new Pose(135, 74, Math.toRadians(270));
-    private final Pose twentySecondPosePreOuttakeFour = new Pose(115, 68, Math.toRadians(270));
-    //TODO OUTTAKE FOUR
-    private final Pose twentyThirdPoseOuttakeFour = new Pose(110, 68, Math.toRadians(270));
-    private final Pose twentyFourthPoseParkControlOne = new Pose(130, 68, Math.toRadians(180));
-    private final Pose twentyFourthPosePark = new Pose(132, 130, Math.toRadians(180));
-
     private PathChain action6PushTwo, action24Park, action11turn, action9PreIntakeOne, action16Turn, action22PreOuttakeFourth, action23OuttakeFourth, action15IntakeTwo, action21turn, action21PostIntakeThree, action20IntakeThree, action19PreIntakeThree, action18OuttakeThree, action17PreOuttakeThree,action16PostIntakeTwo, action14PreIntakeTwo, action12PreOuttakeTwo, action10IntakeOne, action11PostIntakeOne, action8PushThree, action7CurvedForPushThree, action1PreOuttakeOne, action5CurvedForPushTwo, action2OuttakeOne, action13OuttakeTwo, action3DoubleCurvedPrePush, action4PushOne;
-    private DcMotor skibidiSlider = null;
-
 
     public void buildPaths() {
         action1PreOuttakeOne = follower.pathBuilder()
@@ -119,95 +85,6 @@ public class autoTest2 extends OpMode {
                 .addPath(new BezierLine(new Point(sixthPosePostPushTwo), new Point(ninthPosePreIntakeOne)))
                 .setLinearHeadingInterpolation(sixthPosePostPushTwo.getHeading(), ninthPosePreIntakeOne.getHeading())
                 .build();
-
-        action10IntakeOne = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(ninthPosePreIntakeOne), new Point(tenthPoseIntakeOne)))
-                .setLinearHeadingInterpolation(ninthPosePreIntakeOne.getHeading(), tenthPoseIntakeOne.getHeading())
-                .build();
-
-        action11PostIntakeOne = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(tenthPoseIntakeOne), new Point(eleventhPosePostIntakeOne)))
-                .setLinearHeadingInterpolation(tenthPoseIntakeOne.getHeading(), eleventhPosePostIntakeOne.getHeading())
-                .build();
-
-        action11turn = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(eleventhPosePostIntakeOne), new Point(eleventhPosePostIntakeOneTurned)))
-                .setLinearHeadingInterpolation(eleventhPosePostIntakeOne.getHeading(), eleventhPosePostIntakeOneTurned.getHeading())
-                .build();
-
-        action12PreOuttakeTwo = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(eleventhPosePostIntakeOne), new Point(twelfthPosePreOuttakeTwoControlOne), new Point(twelfthPosePreOuttakeTwo)))
-                .setLinearHeadingInterpolation(eleventhPosePostIntakeOne.getHeading(), twelfthPosePreOuttakeTwo.getHeading())
-                .build();
-
-        action13OuttakeTwo = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(twelfthPosePreOuttakeTwo), new Point(thirteenthPoseOuttakeTwo)))
-                .setLinearHeadingInterpolation(twelfthPosePreOuttakeTwo.getHeading(), thirteenthPoseOuttakeTwo.getHeading())
-                .setZeroPowerAccelerationMultiplier(2)
-                .build();
-
-        action14PreIntakeTwo = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(thirteenthPoseOuttakeTwo), new Point(fourteenthPosePreIntakeTwoControlOne), new Point(fourteenthPosePreIntakeTwo)))
-                .setLinearHeadingInterpolation(thirteenthPoseOuttakeTwo.getHeading(), fourteenthPosePreIntakeTwo.getHeading())
-                .build();
-
-        action15IntakeTwo = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(fourteenthPosePreIntakeTwo), new Point(fifteenthPoseIntakeTwo)))
-                .setLinearHeadingInterpolation(fourteenthPosePreIntakeTwo.getHeading(), fifteenthPoseIntakeTwo.getHeading())
-                .build();
-
-        action16PostIntakeTwo = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(fifteenthPoseIntakeTwo), new Point(sixteenthPosePostIntakeTwo)))
-                .setLinearHeadingInterpolation(fifteenthPoseIntakeTwo.getHeading(), sixteenthPosePostIntakeTwo.getHeading())
-                .build();
-
-        action16Turn = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(sixteenthPosePostIntakeTwo), new Point(sixteenthPosePostIntakeTwoTurned)))
-                .setLinearHeadingInterpolation(sixteenthPosePostIntakeTwo.getHeading(), sixteenthPosePostIntakeTwoTurned.getHeading())
-                .build();
-
-        action17PreOuttakeThree = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(sixteenthPosePostIntakeTwo), new Point(seventeenthPosePreOuttakeThreeControlOne), new Point(seventeenthPosePreOuttakeThree)))
-                .setLinearHeadingInterpolation(sixteenthPosePostIntakeTwo.getHeading(), seventeenthPosePreOuttakeThree.getHeading())
-                .build();
-
-        action18OuttakeThree = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(seventeenthPosePreOuttakeThree), new Point(eighteenthPoseOuttakeThree)))
-                .setLinearHeadingInterpolation(seventeenthPosePreOuttakeThree.getHeading(), eighteenthPoseOuttakeThree.getHeading())
-                .setZeroPowerAccelerationMultiplier(2)
-                .build();
-
-        action19PreIntakeThree = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(eighteenthPoseOuttakeThree), new Point(nineteenthPosePreIntakeThreeControlOne), new Point(nineteenthPosePreIntakeThree)))
-                .setLinearHeadingInterpolation(eighteenthPoseOuttakeThree.getHeading(), nineteenthPosePreIntakeThree.getHeading())
-                .build();
-
-        action20IntakeThree = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(nineteenthPosePreIntakeThree), new Point(twentiethPoseIntakeThree)))
-                .setLinearHeadingInterpolation(nineteenthPosePreIntakeThree.getHeading(), twentiethPoseIntakeThree.getHeading())
-                .build();
-
-        action21PostIntakeThree = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(twentiethPoseIntakeThree), new Point(twentyFirstPostPoseIntakeThree)))
-                .setLinearHeadingInterpolation(twentiethPoseIntakeThree.getHeading(), twentyFirstPostPoseIntakeThree.getHeading())
-                .build();
-
-        action22PreOuttakeFourth = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(twentyFirstPostPoseIntakeThree), new Point(twentySecondPosePreOuttakeFourControlOne), new Point(twentySecondPosePreOuttakeFour)))
-                .setLinearHeadingInterpolation(twentyFirstPostPoseIntakeThree.getHeading(), twentySecondPosePreOuttakeFour.getHeading())
-                .build();
-
-        action23OuttakeFourth = follower.pathBuilder()
-                .addPath(new BezierLine(new Point(twentySecondPosePreOuttakeFour), new Point(twentyThirdPoseOuttakeFour)))
-                .setLinearHeadingInterpolation(twentySecondPosePreOuttakeFour.getHeading(), twentyThirdPoseOuttakeFour.getHeading())
-                .build();
-
-        action24Park = follower.pathBuilder()
-                .addPath(new BezierCurve(new Point(twentyThirdPoseOuttakeFour), new Point(twentyFourthPoseParkControlOne), new Point(twentyFourthPosePark)))
-                .setLinearHeadingInterpolation(twentyThirdPoseOuttakeFour.getHeading(), twentyFourthPosePark.getHeading())
-                .setZeroPowerAccelerationMultiplier(7)
-                .build();
-
     }
 
     public void autonomousPathUpdate() {
